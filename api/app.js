6c02dfb9f0ededabb85189bdeaea2a5273fc72a7
AJ Johnson <AJJohnsonUtah@gmail.com>
2024-03-20 14:32:57 -0600

    Changing order to allow larger file uploads

--- api/app.js
+++ api/app.js
@@ -19,11 +19,11 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-app.use(bodyParser.json({ limit: "30mb" }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
